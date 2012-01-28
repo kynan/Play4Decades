b6b80f0634b2506eff68f9fb6071dbfd1bd5e7da
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-01-28 23:03:59 +0000

    Add game.current round and show in index/show view.

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,9 @@ class Game < ActiveRecord::Base
   has_many :teams
   has_one :admin
   attr_accessible :state, :nrounds
+
+  def current_round
+    # FIXME: Do this in a nicer way
+    self.teams.first.try(:team_rounds).try(:count) or 0
+  end
 end
