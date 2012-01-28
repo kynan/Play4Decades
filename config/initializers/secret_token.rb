70045801281ed8d1788bd985b8bbbb61134473ad
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-01-28 17:57:45 +0000

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Play4Decades::Application.config.secret_token = 'bf814b746db0bdad85cce25eb47bb31ff8b6234df34684f66da21d882c853eedfa7d65116b8a9ca75438361c2cde82ea755b8c1d0d272ff48bad9ad3dadaeb39'
