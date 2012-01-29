4055021a4d2fc581b2a56f85e51f711f907902ce
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-01-29 11:25:29 +0000

    Make updating the player round always redirect to the current round.

--- app/controllers/player_rounds_controller.rb
+++ app/controllers/player_rounds_controller.rb
@@ -29,8 +29,10 @@ class PlayerRoundsController < ApplicationController
 
   def update
     @player_round = PlayerRound.find(params[:id])
+    @player = @player_round.player
+    @current_round = @player.current_round
     if @player_round.update_attributes(params[:player_round])
-      redirect_to edit_player_player_round_path(@player_round.player, @player_round), :notice  => "Successfully updated player round."
+      redirect_to edit_player_player_round_path(@player, @current_round)
     else
       render :action => 'edit'
     end
